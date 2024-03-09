7bfb77181af9787a441931cd69c4514bae9f94cc
NestorXDP <nestor.diaz@catolica.edu.sv>
2024-03-09 11:05:03 -0600

    Agregado controlador

--- PARCIAL1B/Controllers/Platos2Controller.cs
+++ PARCIAL1B/Controllers/Platos2Controller.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PARCIAL1B.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http.HttpResults;
+using System.Text.RegularExpressions;
 
 namespace PARCIAL1B.Controllers
 {
@@ -7,5 +11,18 @@ namespace PARCIAL1B.Controllers
     [ApiController]
     public class Platos2Controller : ControllerBase
     {
+        [Route("api/[controller]")]
+        [ApiController]
+        public class platosController : ControllerBase
+        {
+            private readonly parcialContexto _parcialContexto;
+
+            public platosController(parcialContexto parcialContexto)
+            {
+                _parcialContexto = parcialContexto;
+            }
+
+            ////
+        }
     }
 }
