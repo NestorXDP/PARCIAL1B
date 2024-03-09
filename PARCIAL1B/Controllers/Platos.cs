86de8cbb7f7b79e3f57d75c323ab5abd8b618997
NestorXDP <nestor.diaz@catolica.edu.sv>
2024-03-09 10:54:34 -0600

    Creacion de controlador

---
+++ PARCIAL1B/Controllers/Platos.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PARCIAL1B.Controllers
+{
+    public class Platos : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PARCIAL1B/Models/parcialContexto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+namespace PARCIAL1B.Models
+{
+    public class parcialContexto : DbContext
+    {
+        public class parcialContexto : DbContext
+        {
+            public parcialContexto(DbContextOptions<parcialContexto> options) : base(options)
+            {
+            }
+
+            public DbSet<PARCIAL1B> PARCIAL1B { get; set; }
+        }
+}
